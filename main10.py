7e19a58da2cc55bd9146c20f5076bf4b71040c97
Jaithra18 <Jaithrathota@gmail.com>
2024-09-30 18:13:22 +0100

    Add files via upload

---
+++ main10.py
@@ -0,0 +1,21 @@
+a=18
+b=69
+print(id(a))
+print(id(b))
+print(a is b)
+print(a is not b)
+
+x="Hello world"
+y="Hello"
+print(y in x)
+
+a1=10
+b1=5
+print("bitwise and operator",a1&b1)
+print("bitwise or operator",a1|b1)
+d=9
+print("bitwise xor operator",a1^d)
+print("bitwise not operator",~a1)
+x1=3
+print("bitwise << operator",x1<<1)
+print("bitwise >> operator",x1>>1)
